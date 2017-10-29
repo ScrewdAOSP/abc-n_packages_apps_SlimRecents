f1a6f427642fd8c48d51d00afcc062d3787b049e
ezio84 <brabus84@gmail.com>
2017-10-29 19:18:55 +0100

    Fix FC when double tapping for last app and there's only one app
    if we have top task in foreground, and we have no other apps,
    don't trigger last app action
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/systemui/slimrecent/RecentController.java
+++ src/com/android/systemui/slimrecent/RecentController.java
@@ -1011,7 +1011,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
         if (mTasks != null && !mTasks.isEmpty()) {
             if (!mIsTopTaskInForeground) {
                 startApplication(mTasks.get(0));
-            } else if (mTasks.size() > 0) {
+            } else if (mTasks.size() > 1) {
                 startApplication(mTasks.get(1));
             }
         }
